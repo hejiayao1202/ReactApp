4ad13c2393e91451584a670848d4dc13991340d2
何家瑶 <33645036+hejiayao1202@users.noreply.github.com>
2019-11-15 09:48:09 +0800

    Add files via upload

---
+++ build/precache-manifest.95a0049fe3b5982168c21dd7bbefb5b2.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a8957f247b65ead92a750de6a2328b",
+    "url": "index.html"
+  },
+  {
+    "revision": "a9850d7a087dadf18eed",
+    "url": "static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "3d95677aec2f31b75612",
+    "url": "static/css/main.0df1c542.chunk.css"
+  },
+  {
+    "revision": "a9850d7a087dadf18eed",
+    "url": "static/js/2.fd6a708a.chunk.js"
+  },
+  {
+    "revision": "3d95677aec2f31b75612",
+    "url": "static/js/main.9c7a19b2.chunk.js"
+  },
+  {
+    "revision": "ca058ba44d983ac9e1ca",
+    "url": "static/js/runtime-main.ddbf6686.js"
+  },
+  {
+    "revision": "0ca2de90f202290b81838f1c557a61a1",
+    "url": "static/media/1.0ca2de90.jpg"
+  },
+  {
+    "revision": "193a38978ab4cd4ec53e0a94439f36fb",
+    "url": "static/media/2.193a3897.jpg"
+  },
+  {
+    "revision": "b291442f15265dccaad8670fd89a3c47",
+    "url": "static/media/2_1.b291442f.jpg"
+  },
+  {
+    "revision": "bf0c4d1838e01c475906f3bb998fe713",
+    "url": "static/media/2_2.bf0c4d18.jpg"
+  },
+  {
+    "revision": "0de64d833995160a1704b3305eaff6b6",
+    "url": "static/media/2_3.0de64d83.jpg"
+  },
+  {
+    "revision": "0e7bd5bac420713de92af5e4aa035332",
+    "url": "static/media/2_4.0e7bd5ba.jpg"
+  },
+  {
+    "revision": "4f356d2ccc198431795017165a5974bb",
+    "url": "static/media/buy_bottom.4f356d2c.jpg"
+  },
+  {
+    "revision": "3459114c2da33f40e6aa7d390d21e372",
+    "url": "static/media/clock.3459114c.jpg"
+  },
+  {
+    "revision": "93a894154022f4457f0167b0b3d4025b",
+    "url": "static/media/cup.93a89415.jpg"
+  },
+  {
+    "revision": "0c87c43e09367bb0e33448d60ed26c98",
+    "url": "static/media/hot.0c87c43e.jpg"
+  },
+  {
+    "revision": "3f295c6c7f3b4d191ae93e019eb34fa3",
+    "url": "static/media/lg_img1.3f295c6c.jpg"
+  },
+  {
+    "revision": "6c03e83d51b5aa86a513545267c20a8a",
+    "url": "static/media/lg_img2.6c03e83d.jpg"
+  },
+  {
+    "revision": "007ab9f5414882a4482af38099a1227a",
+    "url": "static/media/middle1.007ab9f5.jpg"
+  },
+  {
+    "revision": "c51577831f62990d0db8ec5087e237f6",
+    "url": "static/media/middle2.c5157783.jpg"
+  },
+  {
+    "revision": "b452c12c5b4ade30881687ccc99065e0",
+    "url": "static/media/middle3.b452c12c.jpg"
+  },
+  {
+    "revision": "6e4542773c273fbaa92fd58f827750cb",
+    "url": "static/media/small1.6e454277.jpg"
+  },
+  {
+    "revision": "49cebb924cccedf2bdc868a7fcd32164",
+    "url": "static/media/small2.49cebb92.jpg"
+  },
+  {
+    "revision": "dbcb07ea628c797a24e6e48cfabce89f",
+    "url": "static/media/small3.dbcb07ea.jpg"
+  },
+  {
+    "revision": "36b2850fbd85c11a12f91569b9b3dfaf",
+    "url": "static/media/touxiang1.36b2850f.jpg"
+  },
+  {
+    "revision": "0d2c15740df024e9175a3ebd2ff059db",
+    "url": "static/media/touxiang2.0d2c1574.jpg"
+  },
+  {
+    "revision": "d1ff8e57b4ae3a6959dcf82434421f83",
+    "url": "static/media/xs1.d1ff8e57.jpg"
+  },
+  {
+    "revision": "f83bd4f29f3982bdf8e2f7a5935570f7",
+    "url": "static/media/xs10.f83bd4f2.jpg"
+  },
+  {
+    "revision": "de01716691637f2d376715c3f4c767de",
+    "url": "static/media/xs2.de017166.jpg"
+  },
+  {
+    "revision": "e168b506663b7c0950a74507e57deb07",
+    "url": "static/media/xs3.e168b506.jpg"
+  },
+  {
+    "revision": "3c5805db300220417de9fe6cb1aebc8c",
+    "url": "static/media/xs4.3c5805db.jpg"
+  },
+  {
+    "revision": "196d975a3f6791bb6b3de5078c8dfc5f",
+    "url": "static/media/xs5.196d975a.jpg"
+  },
+  {
+    "revision": "89e2024a83be41758e7004fc5f2b3af9",
+    "url": "static/media/xs6.89e2024a.jpg"
+  },
+  {
+    "revision": "840d54c831ff149bbc87ead3f462b3ca",
+    "url": "static/media/xs7.840d54c8.jpg"
+  },
+  {
+    "revision": "8335ff93064afd170ed6a33d82a61e45",
+    "url": "static/media/xs8.8335ff93.jpg"
+  },
+  {
+    "revision": "4e7622e9fe570f51510bb73a486b45e3",
+    "url": "static/media/xs9.4e7622e9.jpg"
+  }
+]);
